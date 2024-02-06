30e9c421342f82ee42097ee32e06e7b7eca5db8f
Tomer <tomerrim@gmail.com>
2024-02-06 19:31:39 +0200

    added configuration.ini file, made some changes on Event class and on the Dockerfile

--- kafka-producer/event.py
+++ kafka-producer/event.py
@@ -1,23 +1,38 @@
 import datetime
-import random
+from random import randint
 import json
+import configparser
 
+
+# Read configuration from configuration.ini file
+config = configparser.ConfigParser()
+config.read("configuration.ini")
 
 class Event:
-    def __init__(self, reporter_id):
-        self.reporter_id = reporter_id
-        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.metric_id = random.randint(1, 10)
-        self.metric_value = random.randint(1, 100)
-        self.message = "HELLO WORLD"
+    def __init__(self):
+        self.reporter_id = config.getint("event", "initial_reporter_id")
+        self.timestamp = datetime.now().strftime(config.get("Event", "timestamp_format"))
+        self.metric_id = randint(config.getint("Event", "min_metric_id"), config.getint("Event", "max_metric_id"))
+        self.metric_value = randint(config.getint("Event", "min_metric_id"), config.getint("Event", "max_metric_value"))
+        self.message = config.get("Event", "message")
 
     def to_json(self):
-        return json.dumps(
-            {
-                "reporterId": self.reporter_id,
-                "timestamp": self.timestamp,
-                "metricId": self.metric_id,
-                "metricValue": self.metric_value,
-                "message": self.message,
-            }
-        )
+        """
+        Converts the Event object to a JSON string.
+
+        Returns:
+        - str: The JSON representation of the Event.
+        """
+        try:
+            return json.dumps(
+                {
+                    "reporterId": self.reporter_id,
+                    "timestamp": self.timestamp,
+                    "metricId": self.metric_id,
+                    "metricValue": self.metric_value,
+                    "message": self.message,
+                }
+            )
+        except json.JSONDecodeError as e:
+            # Handle JSON serialization errors
+            raise RuntimeError(f"Error converting Event to JSON: {e}")
--- kafka-producer/main.py
+++ kafka-producer/main.py
@@ -1,14 +1,23 @@
-from kafka import KafkaProducer
 import json
 import time
+import configparser
+from kafka import KafkaProducer
 from event import Event
+
+
+# Read configuration from configuration.ini file
+config = configparser.ConfigParser()
+config.read('configuration.ini')
 
 # Define Kafka Configuration
 # Move values later to configuration file
-kafka_config = {"bootstrap_servers": "localhost:9092", "client_id": "python-producer"}
+kafka_config = {
+    "bootstrap_servers": config.get("Kafka", "bootstrap_servers"),
+    "client_id": config.get("Kafka", "client_id")
+}
 
 # Define Kafka Topic
-kafka_topic = "my_topic"
+kafka_topic = config.get("Kafka", "topic")
 
 # Create Kafka producer instance
 producer = KafkaProducer(
@@ -17,19 +26,18 @@ producer = KafkaProducer(
 
 
 try:
-    # change it later
-    reporter_id = 0
-
     while True:
-        reporter_id += 1
-        event = Event(reporter_id)
+        event = Event()
         event_json = event.to_json()
 
         # Produce the event to kafka
-        producer.send(kafka_topic, key=str(reporter_id), value=event_json)
+        producer.send(kafka_topic, key=str(event.reporter_id), value=event_json)
+
+        # Increment reporter_id for the next event
+        event.reporter_id += config.getint("event", "reporter_id_increment")
 
         # wait 1 second before producing the next event
-        time.sleep(1)
+        time.sleep(config.getint("Event", "sleep_time_seconds"))
 except KeyboardInterrupt:
     pass
 finally:
