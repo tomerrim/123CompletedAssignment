58e899c9ddb90ad2e4cfb2b8afbc414066d42767
Tomer <tomerrim@gmail.com>
2024-02-09 11:59:49 +0200

    added redis-etl service

--- redis-etl/main.py
+++ redis-etl/main.py
@@ -1,5 +1,4 @@
 import json
-import threading
 import time
 from pymongo import MongoClient
 from config_loader import config
@@ -22,38 +21,41 @@ redis_port = config["Redis"]["port"]
 redis_sleep_time_seconds = config["Redis"]["sleep_time_seconds"]
 
 timestamp_format = config["Event"]["timestamp_format"]
+timestamp = config["Event"]["timestamp"]
 
 # Create Redis client
 redis_client = Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 def main():
-    etl_thread = threading.Thread(target=run_etl_process, args=(mongo_client,redis_client))
-    etl_thread.start()
-
-
-def run_etl_process(mongo_client, redis_client):
     while True:
         extract_transform_load(mongo_client, redis_client)
         time.sleep(redis_sleep_time_seconds)
 
 
 def extract_transform_load(mongo_client, redis_client):
-    latest_timestamp = get_latest_timestamp(redis_client)
-    new_data_cursor = events_collection.find({"timestamp": {"$gt": latest_timestamp}}).sort("timestamp", 1)
+    latest_timestamp = get_latest_timestamp()
+    timestamp_query = {timestamp: {"$gt": latest_timestamp}} if latest_timestamp else {}
+    new_data_cursor = list(events_collection.find(timestamp_query).sort(timestamp, 1))
     for data in new_data_cursor:
-        redis_key = f"{data['reporterId']}:{data['timestamp'].strftime(timestamp_format)}"
-        redis_value = json.dumps(data)
+        redis_key = f"{data['reporterId']}:{datetime.strftime(data[timestamp],timestamp_format)}"
+        redis_value = json.dumps(data, default=str)
         redis_client.set(redis_key, redis_value)
+        set_latest_timestamp(data[timestamp])
+        print(f"object with key {redis_key} inserted to redis database")
 
 
-def get_latest_timestamp(redis_client):
+def get_latest_timestamp():
     try:
-        keys = redis_client.keys(pattern='*:*')
-        timestamps = [datetime.strftime(key.decode('utf-8').split(':')[1], timestamp_format) for key in keys]
-        return max(timestamps) if timestamps else datetime.min
+        latest_timestamp = redis_client.get('latest_timestamp')
+        latest = datetime.strptime(latest_timestamp, timestamp_format)
+        return latest if latest_timestamp else None
     except Exception as e:
         print(f"Error retrieving latest timestamp from Redis: {e}")
         return datetime.min
+
+
+def set_latest_timestamp(timestamp):
+    redis_client.set('latest_timestamp', timestamp.strftime(timestamp_format))
 
 
 if  __name__ == "__main__":
